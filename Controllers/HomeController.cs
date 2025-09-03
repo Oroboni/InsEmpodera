e345971c2e0e613e44e2ffb64d839eb602207602
ElisaPrestes <bruna.almeida32@fatec.sp.gov.br>
2025-09-03 17:38:55 -0300

    correção

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -105,9 +105,5 @@ public class HomeController : Controller
         return RedirectToAction("Home");
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    
 }
