fdc30d1597dd1c67013526903e49107827d7f0b4
Matheus Cuero <101690604+Oroboni@users.noreply.github.com>
2025-11-25 16:17:33 -0300

    Delet Develop

--- Models/ComunidadeDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Empodera.Models;
-public class ComunidadeDto
-{
-    public int Id { get; set; }
-    public string? Nome { get; set; }
-    public string? Status { get; set; }
-    public string? Ativo { get; set; }
-}
